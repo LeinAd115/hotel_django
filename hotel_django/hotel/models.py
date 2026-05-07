45e621b604be018cbe35aa92ca5ae119327258f1
Joseph <danielngror@gmail.com>
2026-05-06 21:11:21 -0600

    Conexion Django con PostgreSQL y modelo Habitacion

--- hotel_django/hotel/models.py
+++ hotel_django/hotel/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Habitacion(models.Model):
+
+    nombre = models.CharField(max_length=100)
+
+    descripcion = models.TextField()
+
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+
+    tipo = models.CharField(max_length=50)
+
+    imagen = models.URLField()
+
+    def __str__(self):
+        return self.nombre
